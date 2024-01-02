625de49536c9bc65b6ac2f2c7f7d082fcac41049
notlelouch <notlelouchvb@gmail.com>
2024-01-03 03:56:11 +0530

    testing beta

--- app/main.py
+++ app/main.py
@@ -9,8 +9,8 @@ def main():
     args = sys.argv[4:]
 
 # subprocess.run executes the specified command with the provided arguments   
-    completed_process = subprocess.run([command, *args], capture_output=True)
-    print(completed_process.stdout.decode("utf-8"))
+    #completed_process = subprocess.run([command, *args], capture_output=True)
+    #print(completed_process.stdout.decode("utf-8"))
 
     completed_process = subprocess.Popen(
         [command], *args, stdout=subprocess.PIPE, stderr=subprocess.PIPE
