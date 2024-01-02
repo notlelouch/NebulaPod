3bb590abdd013981afb080846f7eff45402df9ea
notlelouch <notlelouchvb@gmail.com>
2024-01-02 23:59:39 +0530

    stage complete

--- app/main.py
+++ app/main.py
@@ -5,9 +5,11 @@ import sys
 def main():
     print("Logs from your program will appear here!")
     
+# Retrieving the command and its arguments from the command-line arguments
     command = sys.argv[3]
     args = sys.argv[4:]
-    
+
+# subprocess.run executes the specified command with the provided arguments   
     completed_process = subprocess.run([command, *args], capture_output=True)
     print(completed_process.stdout.decode("utf-8"))
 
