a8f16ab6cf62c195817da259041f657254c925bf
notlelouch <notlelouchvb@gmail.com>
2024-01-03 03:45:33 +0530

    testing beta

--- app/main.py
+++ app/main.py
@@ -1,24 +1,24 @@
 import subprocess
 import sys
+
+
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    #print("Logs from your program will appear here!")
-    # Uncomment this block to pass the first stage
-    #
-    # print("Logs from your program will appear here!")
+
+# Retrieving the command and its arguments from the command-line arguments
     command = sys.argv[3]
     args = sys.argv[4:]
-    #
+
+# subprocess.run executes the specified command with the provided arguments   
     completed_process = subprocess.run([command, *args], capture_output=True)
     print(completed_process.stdout.decode("utf-8"))
+
     completed_process = subprocess.Popen(
-        [command, *args], stdout=subprocess.PIPE, stderr=subprocess.PIPE
+        [command], *args, stdout=subprocess.PIPE, stderr=subprocess.PIPE
     )
     stdout, stderr = completed_process.communicate()
-    sys.stdout.write(stdout.decode("utf-8"))
 
+    sys.stdout.write(stdout.decode("utf-8"))
     sys.stderr.write(stderr.decode("utf-8"))
 
 if __name__ == "__main__":
-
     main()
