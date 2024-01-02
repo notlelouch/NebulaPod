dfd329e15e35bb881205db9a564e1a9c7a87d91a
notlelouch <notlelouchvb@gmail.com>
2024-01-03 03:14:28 +0530

    test

--- app/main.py
+++ app/main.py
@@ -3,8 +3,7 @@ import sys
 
 
 def main():
-    print("Logs from your program will appear here!")
-    
+
 # Retrieving the command and its arguments from the command-line arguments
     command = sys.argv[3]
     args = sys.argv[4:]
@@ -12,7 +11,6 @@ def main():
 # subprocess.run executes the specified command with the provided arguments   
     completed_process = subprocess.run([command, *args], capture_output=True)
     print(completed_process.stdout.decode("utf-8"))
-
 
 if __name__ == "__main__":
     main()
